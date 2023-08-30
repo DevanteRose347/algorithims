0abf0aaca078977b7ddc349c996291f5fc449262
Devante Rose <drose2021@yahoo.com>
2023-08-30 18:58:18 -0400

    last commit

--- algorithms.py
+++ algorithms.py
@@ -38,3 +38,23 @@ for name in my_W_list:
 print(word_D)
 
 
+## Exercise #3
+
+#Use binary search to return the index of the target num
+
+cool_list = [2, 5, 6, 12, 45, 47, 98, 123, 1000]
+target = 123
+
+def b_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+print(b_search([2, 5, 6, 12, 45, 47, 98, 123, 1000], 123))
